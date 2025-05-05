28f4444bc1a9a06ed7d6374f35f58ad9d7f985e8
Aditya Sood <aditya.sood2@gmail.com>
2025-05-05 03:25:55 -0400

    Rebuild and fix swap app for GitHub Pages

---
+++ swap/client/src/App.tsx
@@ -0,0 +1,36 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ScheduleProvider } from "./context/ScheduleContext";
+import NotFound from "@/pages/not-found";
+import Home from "@/pages/Home";
+import Footer from "@/components/Footer";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <ScheduleProvider>
+          <div className="flex flex-col min-h-screen">
+            <Toaster />
+            <Router />
+            <Footer />
+          </div>
+        </ScheduleProvider>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
